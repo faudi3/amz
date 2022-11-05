81dcddbd2dcc7e9a5f4e0c253f1d28e7914fadc7
faudi <alexnagornyi17@gmail.com>
2022-11-05 22:53:52 +0300

    home page + browser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
 import React from "react";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Routes>
+        <Route
+          path={"/checkout"}
+          element={
+            <>
+              <Header />
+              <h1>hi</h1>
+            </>
+          }
+        ></Route>
+        <Route
+          path={"/"}
+          element={
+            <>
+              <Header /> <Home />
+            </>
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 }
