0fc54c3c6c0ca654d7daf760f3080ef756b3a5f3
faudi <alexnagornyi17@gmail.com>
2022-11-05 20:34:51 +0300

    init

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDMchJ8xgskDStpa9aO7sUnxZDNGtKA9Y",
+    authDomain: "amzn-c17e5.firebaseapp.com",
+    projectId: "amzn-c17e5",
+    storageBucket: "amzn-c17e5.appspot.com",
+    messagingSenderId: "1019250714431",
+    appId: "1:1019250714431:web:6f15dab4989fee6e74158a",
+    measurementId: "G-1R13E0TTP1"
+};
